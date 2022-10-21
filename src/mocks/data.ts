b87cabb9600f576034fabb61386689c4cc22830e
KostaDokosic <kosta99dt@gmail.com>
2022-10-21 12:52:33 +0200

    first commit

---
+++ src/mocks/data.ts
@@ -0,0 +1,12 @@
+import { OrderStatus } from "~/constants/order";
+import { CartItem } from "~/models/CartItem";
+import { Order } from "~/models/Order";
+import { AvailableProduct, Product } from "~/models/Product";
+
+export const products: Product[] = [];
+export const availableProducts: AvailableProduct[] = products.map(
+  (product, index) => ({ ...product, count: index + 1 })
+);
+
+export const cart: CartItem[] = [];
+export const orders: Order[] = [];
